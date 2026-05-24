289eb8a9699bddea9e46f89b13698a8cecfa58ed
Dagemgisila <dagemgisila@gmail.com>
2026-05-24 15:19:50 +0300

    fix issue

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import express from "express";
 import { matchRouter } from "./routes/matches.js";
 import http from "http";
-import { attachWebSocketServer } from "./ws/server.js";
-import { commentaryRouter } from "./routes/commentary.js";
-import cors from "cors"
 
 const app = express();
-const PORT = Number(process.env.PORT || 8000);
-const HOST = process.env.HOST
+const PORT = 8000;
+const HOST = "0.0.0.0"
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+
 // JSON middleware
 app.use(express.json());
 const server = http.createServer(app)
@@ -26,15 +16,9 @@ app.get('/', (req, res) => {
 });
 
 app.use("/matches", matchRouter);
-app.use("/matches/:id/commentary", commentaryRouter);
-
-const { broadcastMatchCreated, broadcastCommentary } = attachWebSocketServer(server);
-app.locals.broadcastMatchCreated = broadcastMatchCreated;
-app.locals.broadcastCommentary = broadcastCommentary;
 
 
 server.listen(PORT, HOST, () => {
-  const baseUrl = HOST === "0.0.0.0" ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+  const baseUrl =HOST=== "0.0.0.0" ?  `http://${HOST}:${PORT}` : `http://localhost:${PORT}`;
   console.log(`Server is running at ${baseUrl}`);
-  console.log(`WebSocket server running at ${baseUrl.replace('http', 'ws')}/ws`)
 });
